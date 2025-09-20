c8459aeebdd18fac3d20bd1f8920f85f65bcd195
Zahra Kazemi <zahra.kazemi1372@yahoo.com>
2025-09-20 00:53:07 -1200

    Time: 427 ms (95.30%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+select
+    project_id,
+    round(avg(experience_years),2) average_years 
+from Employee e
+inner join Project p
+on e.employee_id = p.employee_id 
+group by project_id
+
+
